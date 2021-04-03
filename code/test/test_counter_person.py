ac3f2578cb9e7a4ab13bcab869d9e62f46a9e060
dvalladaresv <d.valladaresvigara@gmail.com>
2021-04-03 17:43:29 +0200

    Test

---
+++ code/test/test_counter_person.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+
+sys.path.append("../affluence_counter")
+from affluence_counter import AffluenceCounter
+
+
+class TestCounterPerson(unittest.TestCase):
+
+    def test_one_person_enter(self):
+        path_video = "../../videos/1_EnterExitCrossingPaths1front.mpg"
+        app = AffluenceCounter(path_video)
+        count_enter, count_noenter = app.process_video()
+        self.assertEqual(count_enter, 1)
+        self.assertEqual(count_noenter, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()  # llama a la clase unittest y ejecuta los test que se tienen definidos
